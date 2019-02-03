381b8ab48028e48ca50fee36d09670d84c80219a
Lucas Kauff <l.kauff@rubika-edu.com>
2019-02-03 02:00:35 +0100

    Proper Starting Points
    HUD still bugged

---
+++ MEMOH/Assets/LucasStuff/Scripts/PlayerStartPoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStartPoint : MonoBehaviour
+{
+    public GameObject thePlayer;
+
+    private void Start()
+    {
+        thePlayer.transform.position = transform.position;
+    }
+}
